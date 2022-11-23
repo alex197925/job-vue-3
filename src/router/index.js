1ee809eb99f14528e8826c29a01f7adae63a3f32
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-11-23 13:21:28 +0100

    Vue Router

---
+++ src/router/index.js
@@ -0,0 +1,56 @@
+/** @format */
+
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import Jobs from "../views/jobs/Jobs.vue";
+import JobDetails from "../views/jobs/JobDetails.vue";
+import NotFoundPage from "../views/NotFoundPage.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeView,
+  },
+
+  {
+    path: "/about",
+    name: "About",
+    component: AboutView,
+  },
+
+  {
+    path: "/jobs",
+    name: "Jobs",
+    component: Jobs,
+  },
+
+  {
+    path: "/jobs/:id",
+    name: "JobDetails",
+    component: JobDetails,
+    props: true,
+  },
+
+  //redirect
+
+  {
+    path: "/all-jobs",
+    redirect: "/jobs",
+  },
+
+  // catchall 404
+  {
+    path: "/:catchAll(.*)*",
+    name: "NotFoundPage",
+    component: NotFoundPage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<!-- @format -->
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit
+      necessitatibus cumque iste veritatis nobis modi soluta? Alias nam
+      accusamus totam sint delectus vel ducimus consectetur illo hic, error
+      saepe soluta explicabo eos officia id blanditiis maiores qui et porro quo
+      consequuntur impedit? Enim, necessitatibus quo dolorem sapiente sit nulla
+      aspernatur porro exercitationem voluptates hic consequatur quis assumenda
+      nostrum est alias repudiandae distinctio voluptas dolores quia quaerat!
+      Minima, incidunt, fugit dolor inventore nihil voluptates aperiam atque qui
+      facilis quo magni. Ducimus corporis dolorum hic ut praesentium iusto, vel
+      placeat dignissimos dolores voluptate harum veniam eaque ipsam, dicta et
+      reiciendis quam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit
+      necessitatibus cumque iste veritatis nobis modi soluta? Alias nam
+      accusamus totam sint delectus vel ducimus consectetur illo hic, error
+      saepe soluta explicabo eos officia id blanditiis maiores qui et porro quo
+      consequuntur impedit? Enim, necessitatibus quo dolorem sapiente sit nulla
+      aspernatur porro exercitationem voluptates hic consequatur quis assumenda
+      nostrum est alias repudiandae distinctio voluptas dolores quia quaerat!
+      Minima, incidunt, fugit dolor inventore nihil voluptates aperiam atque qui
+      facilis quo magni. Ducimus corporis dolorum hic ut praesentium iusto, vel
+      placeat dignissimos dolores voluptate harum veniam eaque ipsam, dicta et
+      reiciendis quam!
+    </p>
+  </div>
+</template>
